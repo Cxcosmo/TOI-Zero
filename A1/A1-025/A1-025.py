25fc14e0c0e85d76431741fa4149e7e535ca5416
/ᐠ. ｡.ᐟ\ᵐᵉᵒʷˎˊ˗ <thanapon9114@gmail.com>
2025-06-30 01:50:47 +0700

    fix : Text

--- A1/A1-025/A1-025.py
+++ A1/A1-025/A1-025.py
@@ -2,23 +2,23 @@ def main() :
     card = input()
     name_card = ""
     if card[:-1] in ("a", "A") :
-        name_card += "ace"
+        name_card += "Ace"
     elif card[:-1] in ("j", "J") :
-        name_card += "jack"
+        name_card += "Jack"
     elif card[:-1] in ("q", "Q") :
-        name_card += "queen"
+        name_card += "Queen"
     elif card[:-1] in ("k", "K") :
-        name_card += "king"
+        name_card += "King"
     else :
         name_card += card[:-1]
     name_card += " of "
     if card[-1] in ("d", "D") :
-        name_card += "diamonds"
+        name_card += "Diamonds"
     elif card[-1] in ("h", "H") :
-        name_card += "hearts"
+        name_card += "Hearts"
     elif card[-1] in ("s", "S") :
-        name_card += "spades"
+        name_card += "Spades"
     elif card[-1] in ("c", "C") :
-        name_card += "clubs"
+        name_card += "Clubs"
     print(name_card)
 main()
